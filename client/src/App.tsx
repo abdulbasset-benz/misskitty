00693d1a03956129b6058b6dc7c9024cf0fe0866
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-19 19:48:56 +0100

    Add products page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Layout from "@/layouts/Layout";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* <Route path="about" element={<Products />} /> */}
+        <Route path="products" element={<Products />} />
         
       </Route>
     </Routes>
