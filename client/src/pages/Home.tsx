5706342cf8f3c6d5d52f5cd6d5b7431dce75688c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-20 21:03:11 +0100

    Minor chore

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import Collection from "@/components/Collection";
+import Banner from "@/components/Banner";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <Collection/>
+      <Banner/>
     </div>
   );
 };
