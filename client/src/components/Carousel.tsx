68314acfd9b7dc42fe947f5b3184ac196dd64cf0
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-20 20:17:15 +0100

    minor changes

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 const Carousel = () => {
   return (
     <div className="absolute bottom-0 left-0 w-full overflow-hidden py-6 bg-transparent">
-      <div className="flex whitespace-nowrap items-center gap-8 text-4xl font-medium overflow-hidden font-fatface">
+      <div className="flex whitespace-nowrap items-center gap-8 text-4xl overflow-hidden font-fatface">
         <Marquee className="overflow-hidden" pauseOnHover autoFill>
           <span className="md:text-7xl text-[#FFDBB6] pl-3">
             ELEGANCE • STYLE • LUXURY • FOREVER •
