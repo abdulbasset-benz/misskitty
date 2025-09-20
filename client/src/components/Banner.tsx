2993641690e19e425766ed98143d8c8715f75118
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-20 21:02:49 +0100

    Implement the banner section

---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,27 @@
+import mesh from "@/assets/mesh-gradient.svg";
+import { Button } from "./ui/button";
+import { Link } from "react-router";
+
+const Banner = () => {
+  return (
+    <div className="relative w-screen md:w-[50rem] mx-auto h-48 md:h-64 lg:h-80 overflow-hidden md:rounded-3xl  flex justify-center my-10">
+      {/* Background Image */}
+      <img src={mesh} alt="" className="w-full h-full object-cover" />
+
+      {/* Content Overlay */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 py-4">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white mb-4 font-aboreto">
+          Elegance is awaiting, <span className="text-pink-100">Miss Kitty</span> 
+        </h2>
+        
+        <Button asChild className="bg-[#f7ce83] text-black px-6 py-3 md:px-8 md:py-6 rounded-full font-medium hover:bg-[#f5c563] transition-colors">
+          <Link to="/products" className="text-lg md:text-xl">
+            Shop Now
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
