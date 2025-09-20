d5fb13ca7b0f5477a4baa9380f5de11d666b4925
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-20 20:18:45 +0100

    minor changes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import Collection from "@/components/Collection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Collection/>
     </div>
   );
 };
