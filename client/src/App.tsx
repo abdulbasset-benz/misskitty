8c8327dfc4ceb776fd34569a4b76143fe64f3c9f
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-22 13:32:54 +0100

    change the react router structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
-import { Route, Routes } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
 import Layout from "@/layouts/Layout";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import OrderPage from "./pages/OrderPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<OrderPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/OrderPage.tsx
@@ -0,0 +1,8 @@
+
+const OrderPage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center font-bold text-8xl">Orders Page</div>
+  )
+}
+
+export default OrderPage
