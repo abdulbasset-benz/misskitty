5d8a873cee93044db359efbb2ffeeb00bf09d80d
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-24 17:19:26 +0100

    implement remaining product crud routes

--- server/src/routes/productsRoutes.js
+++ server/src/routes/productsRoutes.js
@@ -3,12 +3,21 @@ import { upload } from "../processImage.js";
 import {
   createProduct,
   getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { adminAuth } from "../middlewares/adminAuth.js";
 
 const router = express.Router();
 
-router.post('/products', upload.array('images', 5), createProduct);
-
+// Public routes
 router.get("/products", getProducts);
+router.get("/products/:id", getProductById);
+
+// Admin-only routes
+router.post("/products", adminAuth, upload.array("images", 5), createProduct);
+router.put("/products/:id", adminAuth, upload.array("images", 5), updateProduct);
+router.delete("/products/:id", adminAuth, deleteProduct);
 
 export default router;
