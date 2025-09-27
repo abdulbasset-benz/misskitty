d419174be9a9d4a12fe907fcb3f27471d3a41d1d
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-27 19:26:26 +0100

    add login logout routes

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { loginAdmin } from "../controllers/adminController.js";
+import { loginAdmin, logoutAdmin } from "../controllers/adminController.js";
+import { adminAuth } from "../middlewares/adminAuth.js";
 
 const router = express.Router();
 
 // Public login route
 router.post("/login", loginAdmin);
+router.post("/logout",adminAuth, logoutAdmin);
 
 export default router;
