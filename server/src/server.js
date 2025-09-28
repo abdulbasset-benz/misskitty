1580a2d5aa505d09205a34a21325dffd5884f397
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-09-28 22:34:01 +0100

    add cookie parser middleware

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
 import productsRoutes from "./routes/productsRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
@@ -22,6 +23,7 @@ app.use(
   })
 );
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
