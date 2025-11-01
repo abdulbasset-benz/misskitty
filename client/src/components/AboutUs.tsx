985128522aa2a02ee83578441234200e03a1e9be
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-11-01 08:52:05 +0100

    Import gsap and plugins needed

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -1,5 +1,12 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
 import woman from "@/assets/woman.png";
 import scissors from "@/assets/scissors.png";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 const AboutUs = () => {
   return (
@@ -32,21 +39,25 @@ const AboutUs = () => {
               className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out"
               loading="lazy"
               decoding="async"
+              id="image"
             />
           </div>
 
           {/* Text Right */}
           <div className="md:w-1/2 w-full space-y-4">
-            <h2 className="text-3xl md:text-4xl font-semibold font-serif tracking-wide">
+            <h2
+              className="text-3xl md:text-4xl font-normal font-serif tracking-wide"
+              id="title"
+            >
               The Beginning of Our Journey
             </h2>
             <div className="w-16 h-[2px] bg-[#f7ce83] mb-4"></div>
-            <p className="text-gray-700 leading-relaxed text-lg">
+            <p className="text-gray-700 leading-relaxed text-lg" id="subtitle">
               Our story began in the heart of Algeria — where craftsmanship is a
-              heritage and style is a way of life. Inspired by local artisans and
-              everyday beauty, we design clothing that celebrates tradition while
-              embracing modern comfort. Every stitch tells a story of pride,
-              patience, and passion.
+              heritage and style is a way of life. Inspired by local artisans
+              and everyday beauty, we design clothing that celebrates tradition
+              while embracing modern comfort. Every stitch tells a story of
+              pride, patience, and passion.
             </p>
           </div>
         </div>
@@ -55,16 +66,19 @@ const AboutUs = () => {
         <div className="flex flex-col-reverse md:flex-row items-center gap-12 max-w-7xl mx-auto">
           {/* Text Left */}
           <div className="md:w-1/2 w-full space-y-4">
-            <h2 className="text-3xl md:text-4xl font-semibold font-serif tracking-wide">
+            <h2
+              className="text-3xl md:text-4xl font-normal font-serif tracking-wide"
+              id="title"
+            >
               Who We Are
             </h2>
             <div className="w-16 h-[2px] bg-[#f7ce83] mb-4"></div>
-            <p className="text-gray-700 leading-relaxed text-lg">
+            <p className="text-gray-700 leading-relaxed text-lg" id="subtitle">
               We’re more than a fashion brand — we’re a reflection of Algeria’s
-              spirit. Our collections blend everyday ease with timeless elegance,
-              crafted for women who move with confidence and live with purpose.
-              From morning markets to evening gatherings, we dress the rhythm of
-              real life with authenticity and charm.
+              spirit. Our collections blend everyday ease with timeless
+              elegance, crafted for women who move with confidence and live with
+              purpose. From morning markets to evening gatherings, we dress the
+              rhythm of real life with authenticity and charm.
             </p>
           </div>
 
@@ -76,6 +90,7 @@ const AboutUs = () => {
               className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out"
               loading="lazy"
               decoding="async"
+              id="image"
             />
           </div>
         </div>
