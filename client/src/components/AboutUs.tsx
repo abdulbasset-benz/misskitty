b894f74adfb8804e1487a2a5671c49142124db0a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-11-02 11:48:32 +0100

    Change ids to classes for html best practice

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -11,97 +11,100 @@ gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 const AboutUs = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  useGSAP(() => {
-    const sections = gsap.utils.toArray("#section");
-    gsap.set("#image", {
-      clipPath: "inset(0 100% 0 0)",
-    });
-
-    const headingSplit = SplitText.create(".heading", {
-      type: "words",
-      mask: "words",
-    });
-    const subHeadingSplit = SplitText.create(".subheading", {
-      type: "words",
-      mask: "words",
-    });
-
-    gsap.from(subHeadingSplit.words, {
-      yPercent: 120,
-      duration: 1,
-      ease: "power4.out",
-      stagger: 0.08,
-      scrollTrigger: {
-        trigger: ".subheading",
-        start: "top 80%",
-        end: "bottom 50%",
-        scrub: 1,
-      },
-    });
-    gsap.from(headingSplit.words, {
-      yPercent: 120,
-      duration: 1,
-      ease: "power4.out",
-      stagger: 0.08,
-      scrollTrigger: {
-        trigger: ".heading",
-        start: "top 80%",
-        end: "bottom 50%",
-        scrub: 1,
-      },
-    });
-    sections.forEach((section) => {
-      const el = section as Element;
-      const title = el.querySelector("#title");
-      const subtitle = el.querySelector("#subtitle");
-      const image = el.querySelector("#image");
-
-      const titleSplit = SplitText.create(title, {
-        type: "lines",
-        mask: "lines",
+  useGSAP(
+    () => {
+      const sections = gsap.utils.toArray("#section");
+      gsap.set(".image", {
+        clipPath: "inset(0 100% 0 0)",
       });
 
-      const subtitleSplit = SplitText.create(subtitle, {
-        type: "lines",
-        mask: "lines",
+      const headingSplit = SplitText.create(".heading", {
+        type: "words",
+        mask: "words",
+      });
+      const subHeadingSplit = SplitText.create(".subheading", {
+        type: "words",
+        mask: "words",
       });
 
-      gsap.to(image, {
-        clipPath: "inset(0 0% 0 0)",
-        duration: 1,
-        ease: "power4.inOut",
-        scrollTrigger: {
-          trigger: image,
-          start: "top 80%",
-          end: "bottom 50%",
-        },
-      });
-
-      gsap.from(titleSplit.lines, {
+      gsap.from(subHeadingSplit.words, {
         yPercent: 120,
         duration: 1,
         ease: "power4.out",
+        stagger: 0.08,
         scrollTrigger: {
-          trigger: title,
+          trigger: ".subheading",
           start: "top 80%",
           end: "bottom 50%",
           scrub: 1,
         },
       });
-
-      gsap.from(subtitleSplit.lines, {
+      gsap.from(headingSplit.words, {
         yPercent: 120,
         duration: 1,
         ease: "power4.out",
+        stagger: 0.08,
         scrollTrigger: {
-          trigger: subtitle,
+          trigger: ".heading",
           start: "top 80%",
           end: "bottom 50%",
           scrub: 1,
         },
       });
-    });
-  }, {scope: containerRef});
+      sections.forEach((section) => {
+        const el = section as Element;
+        const title = el.querySelector(".title");
+        const subtitle = el.querySelector(".subtitle");
+        const image = el.querySelector(".image");
+
+        const titleSplit = SplitText.create(title, {
+          type: "lines",
+          mask: "lines",
+        });
+
+        const subtitleSplit = SplitText.create(subtitle, {
+          type: "lines",
+          mask: "lines",
+        });
+
+        gsap.to(image, {
+          clipPath: "inset(0 0% 0 0)",
+          duration: 1,
+          ease: "power4.inOut",
+          scrollTrigger: {
+            trigger: image,
+            start: "top 80%",
+            end: "bottom 50%",
+          },
+        });
+
+        gsap.from(titleSplit.lines, {
+          yPercent: 120,
+          duration: 1,
+          ease: "power4.out",
+          scrollTrigger: {
+            trigger: title,
+            start: "top 80%",
+            end: "bottom 50%",
+            scrub: 1,
+          },
+        });
+
+        gsap.from(subtitleSplit.lines, {
+          yPercent: 120,
+          duration: 1,
+          ease: "power4.out",
+          scrollTrigger: {
+            trigger: subtitle,
+            start: "top 80%",
+            end: "bottom 50%",
+            scrub: 1,
+          },
+        });
+      });
+    },
+    { scope: containerRef }
+  );
   return (
     <div
       className="mt-8 flex flex-col gap-24 bg-gradient-to-b from-white to-[#fefaf2] text-gray-800"
@@ -113,16 +116,10 @@ const AboutUs = () => {
         <h2 className="uppercase text-xs md:text-sm tracking-[0.3em] text-gray-500 mb-4">
           Get to know us
         </h2>
-        <h1
-          className="font-serif text-5xl md:text-7xl font-light mb-6 leading-tight heading"
-          
-        >
+        <h1 className="font-serif text-5xl md:text-7xl font-light mb-6 leading-tight heading">
           Where <span className="italic font-light">Comfort</span> Meets Charm
         </h1>
-        <p
-          className="font-sans max-w-2xl text-gray-600 text-lg leading-relaxed subheading"
-          
-        >
+        <p className="font-sans max-w-2xl text-gray-600 text-lg leading-relaxed subheading">
           Rooted in Algerian heritage, we craft everyday garments that blend
           softness, confidence, and effortless grace — made for real moments and
           timeless style.
@@ -141,23 +138,19 @@ const AboutUs = () => {
             <img
               src={scissors}
               alt="Scissors symbolizing craftsmanship"
-              className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out"
+              className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out image"
               loading="lazy"
               decoding="async"
-              id="image"
             />
           </div>
 
           {/* Text Right */}
           <div className="md:w-1/2 w-full space-y-4">
-            <h2
-              className="text-3xl md:text-7xl font-normal font-serif leading-tight tracking-wide"
-              id="title"
-            >
+            <h2 className="text-3xl md:text-7xl font-normal font-serif leading-tight tracking-wide title">
               The Beginning of Our Journey
             </h2>
-            <div className="w-16 h-[2px] bg-[#f7ce83] mb-4"></div>
-            <p className="text-gray-700 leading-relaxed text-lg" id="subtitle">
+            <div className="w-16 h-[2px] bg-[#f7ce83] mb-4 "></div>
+            <p className="text-gray-700 leading-relaxed text-lg subtitle">
               Our story began in the heart of Algeria — where craftsmanship is a
               heritage and style is a way of life. Inspired by local artisans
               and everyday beauty, we design clothing that celebrates tradition
@@ -174,14 +167,11 @@ const AboutUs = () => {
         >
           {/* Text Left */}
           <div className="md:w-1/2 w-full space-y-4">
-            <h2
-              className="text-3xl md:text-7xl font-normal font-serif tracking-wide"
-              id="title"
-            >
+            <h2 className="text-3xl md:text-7xl font-normal font-serif tracking-wide title">
               Who We Are
             </h2>
             <div className="w-16 h-[2px] bg-[#f7ce83] mb-4"></div>
-            <p className="text-gray-700 leading-relaxed text-lg" id="subtitle">
+            <p className="text-gray-700 leading-relaxed text-lg subtitle">
               We’re more than a fashion brand — we’re a reflection of Algeria’s
               spirit. Our collections blend everyday ease with timeless
               elegance, crafted for women who move with confidence and live with
@@ -195,10 +185,9 @@ const AboutUs = () => {
             <img
               src={woman}
               alt="Algerian fashion designer"
-              className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out"
+              className="w-full h-full object-cover scale-100 hover:scale-105 transition-transform duration-700 ease-out image"
               loading="lazy"
               decoding="async"
-              id="image"
             />
           </div>
         </div>
