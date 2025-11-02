15fff80898e59549b96bede2eaeef445758c3ea5
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-11-02 11:42:49 +0100

    Implement animations using gsap

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -9,18 +10,119 @@ import scissors from "@/assets/scissors.png";
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 const AboutUs = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useGSAP(() => {
+    const sections = gsap.utils.toArray("#section");
+    gsap.set("#image", {
+      clipPath: "inset(0 100% 0 0)",
+    });
+
+    const headingSplit = SplitText.create(".heading", {
+      type: "words",
+      mask: "words",
+    });
+    const subHeadingSplit = SplitText.create(".subheading", {
+      type: "words",
+      mask: "words",
+    });
+
+    gsap.from(subHeadingSplit.words, {
+      yPercent: 120,
+      duration: 1,
+      ease: "power4.out",
+      stagger: 0.08,
+      scrollTrigger: {
+        trigger: ".subheading",
+        start: "top 80%",
+        end: "bottom 50%",
+        scrub: 1,
+      },
+    });
+    gsap.from(headingSplit.words, {
+      yPercent: 120,
+      duration: 1,
+      ease: "power4.out",
+      stagger: 0.08,
+      scrollTrigger: {
+        trigger: ".heading",
+        start: "top 80%",
+        end: "bottom 50%",
+        scrub: 1,
+      },
+    });
+    sections.forEach((section) => {
+      const el = section as Element;
+      const title = el.querySelector("#title");
+      const subtitle = el.querySelector("#subtitle");
+      const image = el.querySelector("#image");
+
+      const titleSplit = SplitText.create(title, {
+        type: "lines",
+        mask: "lines",
+      });
+
+      const subtitleSplit = SplitText.create(subtitle, {
+        type: "lines",
+        mask: "lines",
+      });
+
+      gsap.to(image, {
+        clipPath: "inset(0 0% 0 0)",
+        duration: 1,
+        ease: "power4.inOut",
+        scrollTrigger: {
+          trigger: image,
+          start: "top 80%",
+          end: "bottom 50%",
+        },
+      });
+
+      gsap.from(titleSplit.lines, {
+        yPercent: 120,
+        duration: 1,
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: title,
+          start: "top 80%",
+          end: "bottom 50%",
+          scrub: 1,
+        },
+      });
+
+      gsap.from(subtitleSplit.lines, {
+        yPercent: 120,
+        duration: 1,
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: subtitle,
+          start: "top 80%",
+          end: "bottom 50%",
+          scrub: 1,
+        },
+      });
+    });
+  }, {scope: containerRef});
   return (
-    <div className="mt-8 flex flex-col gap-24 bg-gradient-to-b from-white to-[#fefaf2] text-gray-800">
+    <div
+      className="mt-8 flex flex-col gap-24 bg-gradient-to-b from-white to-[#fefaf2] text-gray-800"
+      ref={containerRef}
+    >
       {/* Intro Section */}
       <div className="flex flex-col items-center text-center px-6">
         <div className="w-24 h-[2px] bg-gradient-to-r from-transparent via-[#f7ce83] to-transparent mb-6"></div>
         <h2 className="uppercase text-xs md:text-sm tracking-[0.3em] text-gray-500 mb-4">
           Get to know us
         </h2>
-        <h1 className="font-serif text-5xl md:text-7xl font-light mb-6 leading-tight">
+        <h1
+          className="font-serif text-5xl md:text-7xl font-light mb-6 leading-tight heading"
+          
+        >
           Where <span className="italic font-light">Comfort</span> Meets Charm
         </h1>
-        <p className="font-sans max-w-2xl text-gray-600 text-lg leading-relaxed">
+        <p
+          className="font-sans max-w-2xl text-gray-600 text-lg leading-relaxed subheading"
+          
+        >
           Rooted in Algerian heritage, we craft everyday garments that blend
           softness, confidence, and effortless grace — made for real moments and
           timeless style.
@@ -30,7 +132,10 @@ const AboutUs = () => {
       {/* Main Sections */}
       <section className="container mx-auto px-4 py-8 space-y-32">
         {/* Section 1 */}
-        <div className="flex flex-col md:flex-row items-center gap-12 max-w-7xl mx-auto">
+        <div
+          className="flex flex-col md:flex-row items-center gap-12 max-w-7xl mx-auto"
+          id="section"
+        >
           {/* Image Left */}
           <div className="md:w-1/2 w-full overflow-hidden rounded-2xl shadow-md hover:shadow-xl transition-all duration-500 ease-in-out">
             <img
@@ -46,7 +151,7 @@ const AboutUs = () => {
           {/* Text Right */}
           <div className="md:w-1/2 w-full space-y-4">
             <h2
-              className="text-3xl md:text-4xl font-normal font-serif tracking-wide"
+              className="text-3xl md:text-7xl font-normal font-serif leading-tight tracking-wide"
               id="title"
             >
               The Beginning of Our Journey
@@ -63,11 +168,14 @@ const AboutUs = () => {
         </div>
 
         {/* Section 2 */}
-        <div className="flex flex-col-reverse md:flex-row items-center gap-12 max-w-7xl mx-auto">
+        <div
+          className="flex flex-col-reverse md:flex-row items-center gap-12 max-w-7xl mx-auto"
+          id="section"
+        >
           {/* Text Left */}
           <div className="md:w-1/2 w-full space-y-4">
             <h2
-              className="text-3xl md:text-4xl font-normal font-serif tracking-wide"
+              className="text-3xl md:text-7xl font-normal font-serif tracking-wide"
               id="title"
             >
               Who We Are
